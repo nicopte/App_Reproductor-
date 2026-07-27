0132e3e1178f2ae289496e1900c249765e100d16
nicopte <galucamendez@gmail.com>
2026-07-27 08:48:36 -0300

    chore: sacar archivos internos del scaffold

--- generate-albums.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-z-ai image -p "Hip hop album cover, street art graffiti style, bold colors, urban aesthetic, spray paint effects" -o "/home/z/my-project/public/images/albums/album-5.png" -s 1024x1024
-z-ai image -p "Classical music album cover, orchestra in concert hall, warm golden light, elegant and refined atmosphere" -o "/home/z/my-project/public/images/albums/album-6.png" -s 1024x1024
